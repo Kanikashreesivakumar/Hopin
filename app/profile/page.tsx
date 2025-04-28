3b35232ecbc3fb217e22971d15b5aa5c4fdb527f
Em-Vi <musthumuth1107@gmail.com>
2025-04-29 00:52:41 +0400

    removed unnecessary files, planned new user flow, made new authentication, lets go

--- app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-export async function signOut() {
-  // Mock implementation of signOut function
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("User signed out");
-      resolve(true);
-    }, 1000);
-  });
-}
